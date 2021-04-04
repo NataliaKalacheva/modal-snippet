57a0d1b694a8dc5b3640edca5d2c46ce925b7f7c
Natalie <kalachevan615@gmail.com>
2021-04-04 14:02:24 +0700

    bind/unbind events, active source area-expanded toggle, kclose on esc

--- modal.js
+++ modal.js
@@ -3,12 +3,11 @@
 class Modal {
   constructor(id, name, options) {
     const defaults = {
-      close: ".js-modal-close",
-      open: ".js-modal-open-" + name,
+      close: "[data-modal-close]",
+      open: `[data-modal-open="${name}"`,
       openClass: "modal--is-active",
       closingClass: "modal--is-closing",
       bodyOpenClass: "modal-open",
-      bodyOpenSolidClass: "modal-open--solid",
       bodyClosingClass: "modal-closing",
       closeOffContentClick: true,
     };
@@ -19,13 +18,11 @@ class Modal {
       return false;
     }
 
+    this.activeSource = null;
     this.modalContent = this.modal.querySelector(".modal__inner");
     this.config = Object.assign(defaults, options);
     this.modalIsOpen = false;
-    this.focusOnOpen = this.config.focusIdOnOpen
-      ? document.getElementById(this.config.focusIdOnOpen)
-      : this.modal;
-    this.isSolid = this.config.solid;
+    this.onEscDown = this.onEscDown.bind(this);
 
     this.init();
   }
@@ -40,30 +37,22 @@ class Modal {
     });
   }
   open(evt) {
-    // Keep track if modal was opened from a click, or called by another function
     let externalCall = false;
 
-    // don't open an opened modal
     if (this.modalIsOpen) {
       return;
     }
 
-    // Prevent following href if link is clicked
     if (evt) {
       evt.preventDefault();
     } else {
       externalCall = true;
     }
 
-    // Without this, the modal opens, the click event bubbles up to $nodes.page
-    // which closes the modal.
     if (evt && evt.stopPropagation) {
       evt.stopPropagation();
-      // save the source of the click, we'll focus to this on close
-      this.activeSource = evt.currentTarget.setAttribute(
-        "aria-expanded",
-        "true"
-      );
+      this.activeSource = evt.currentTarget;
+      this.activeSource.setAttribute("aria-expanded", "true");
     }
 
     if (this.modalIsOpen && !externalCall) {
@@ -74,31 +63,27 @@ class Modal {
 
     document.documentElement.classList.add(this.config.bodyOpenClass);
 
-    if (this.isSolid) {
-      document.documentElement.classList.add(this.config.bodyOpenSolidClass);
-    }
-
     this.modalIsOpen = true;
 
     document.dispatchEvent(new CustomEvent("modalOpen"));
     document.dispatchEvent(new CustomEvent("modalOpen." + this.id));
+
+    this.bindEvents();
   }
   close(evt) {
-    // don't close a closed modal
+    console.log("WORK");
     if (!this.modalIsOpen) {
       return;
     }
 
-    // Do not close modal if click happens inside modal content
     if (evt) {
-      if (evt.target.closest(".js-modal-close")) {
-        // Do not close if using the modal close button
+      console.log("HERE", evt);
+      if (evt.target.closest("[data-modal-close]")) {
       } else if (evt.target.closest(".modal__inner")) {
         return;
       }
     }
 
-    // deselect any focused form elements
     document.activeElement.blur();
 
     this.modal.classList.remove(this.config.openClass);
@@ -107,27 +92,36 @@ class Modal {
     document.documentElement.classList.remove(this.config.bodyOpenClass);
     document.documentElement.classList.add(this.config.bodyClosingClass);
 
-    window.setTimeout(
-      function () {
-        document.documentElement.classList.remove(this.config.bodyClosingClass);
-        this.modal.classList.remove(this.config.closingClass);
-        if (
-          this.activeSource &&
-          this.activeSource.getAttribute("aria-expanded")
-        ) {
-          this.activeSource.setAttribute("aria-expanded", "false").focus();
-        }
-      }.bind(this),
-      500
-    ); // modal close css transition
+    window.setTimeout(() => {
+      document.documentElement.classList.remove(this.config.bodyClosingClass);
+      this.modal.classList.remove(this.config.closingClass);
 
-    if (this.isSolid) {
-      document.documentElement.classList.remove(this.config.bodyOpenSolidClass);
-    }
+      if (
+        this.activeSource &&
+        this.activeSource.getAttribute("aria-expanded")
+      ) {
+        this.activeSource.setAttribute("aria-expanded", "false");
+        this.activeSource.focus();
+      }
+    }, 500);
 
     this.modalIsOpen = false;
 
     document.dispatchEvent(new CustomEvent("modalClose." + this.id));
+
+    this.unbindEvents();
+  }
+  bindEvents() {
+    document.addEventListener("keyup", this.onEscDown);
+  }
+  unbindEvents() {
+    document.removeEventListener("keyup", this.onEscDown);
+  }
+  onEscDown(evt) {
+    console.log(evt);
+    if (evt.keyCode === 27) {
+      this.close();
+    }
   }
 }
 
